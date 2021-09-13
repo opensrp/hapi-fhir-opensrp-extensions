2f3ac50068045489fdc07542558dc4d4776c8fe0
Benjamin Mwalimu Mulyungi <dubdabasoduba@gmail.com>
2021-09-13 05:16:44 +0300

    :art: Refactor the repo structure to add submodules

---
+++ location/src/main/java/org/smartregister/extension/utils/Constants.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.smartregister.extension.utils;
+
+public interface Constants {
+
+    String SLASH_UNDERSCORE = "/_";
+    String LOCATION = "Location";
+    String FORWARD_SLASH = "/";
+    String IDENTIFIER = "identifier";
+    String LOCATION_RESOURCE_NOT_FOUND = "Location Resource : Not Found";
+    String LOCATION_RESOURCE = "Location Resource : ";
+    String PART_OF = "partof";
+}
