d9ff67692a87818b70238c4602c6557f4027d34e
Reham Muzzamil <reham.muzzamil@venturedive.com>
2021-11-01 13:00:46 +0500

    Fix in Location Hierarchy Bug

--- location/src/main/java/org/smartregister/extension/model/Tree.java
+++ location/src/main/java/org/smartregister/extension/model/Tree.java
@@ -207,10 +207,8 @@ public class Tree extends Type implements ICompositeType {
             return listOfNodes.getTreeNode();
 
         } else {
-            if (listOfNodes != null
-                    && listOfNodes.getTreeNode() != null
-                    && listOfNodes.getTreeNode().getChildren() != null) {
-                return recursivelyFindNode(idString, listOfNodes.getTreeNode().getChildren());
+            if (listOfNodes != null && listOfNodes.getTreeNode() != null) {
+                return listOfNodes.getTreeNode().findChild(idString);
             }
         }
         return null;
