dff62d68de742ad98ce8d33db4aedbcf86e8cfc1
Benjamin Mwalimu Mulyungi <dubdabasoduba@gmail.com>
2023-01-30 23:22:23 +0000

    :bug: Update the code to be use the practtionerRole & practitionerId

--- practitioner/src/main/java/org/smartregister/extension/rest/PractitionerDetailsResourceProvider.java
+++ practitioner/src/main/java/org/smartregister/extension/rest/PractitionerDetailsResourceProvider.java
@@ -359,7 +359,7 @@ public class PractitionerDetailsResourceProvider implements IResourceProvider {
     private List<PractitionerRole> mapToPractitionerRoles(List<IBaseResource> practitionerRoles) {
         List<PractitionerRole> practitionerRoleList = new ArrayList<>();
         PractitionerRole practitionerRoleObj;
-        for (IBaseResource practitionerRole : practitionerRoleList) {
+        for (IBaseResource practitionerRole : practitionerRoles) {
             practitionerRoleObj = (PractitionerRole) practitionerRole;
             practitionerRoleList.add(practitionerRoleObj);
         }
@@ -485,11 +485,18 @@ public class PractitionerDetailsResourceProvider implements IResourceProvider {
         TokenAndListParam codeListParam = new TokenAndListParam();
         TokenOrListParam coding = new TokenOrListParam();
         TokenParam code = new TokenParam();
+        
+        //Adding the code to the search parameters
         code.setValue("405623001");
         code.setSystem("http://snomed.info/sct");
         coding.add(code);
         codeListParam.addAnd(coding);
         groupSearchParameterMap.add(CODE, codeListParam);
+        code.setValue(practitionerId.toString());
+        
+        //Adding the practitioner Id to the search parameters.
+        codeListParam.addAnd(code);
+        groupSearchParameterMap.add(MEMBER, codeListParam);
         IBundleProvider groupsBundle = groupIFhirResourceDao.search(groupSearchParameterMap);
         return groupsBundle != null
                 ? groupsBundle.getResources(0, groupsBundle.size())
